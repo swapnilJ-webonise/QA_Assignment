fd6cc7e3013d028a1b6110ce14bd2abe449bf723
swapnilJ.webonise <swapnil.jejurkar@weboniselab.com>
2018-10-18 17:53:06 +0530

    manual testing and automation testing assignment

--- ChromeBrowserLaunch.java
+++ ChromeBrowserLaunch.java
@@ -1,60 +1,83 @@
 package selenium.test.one;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class ChromeBrowserLaunch implements Runnable{
+public class ChromeBrowserLaunch implements Runnable {
 
-    public static void main(String[] args) {
-        
-       try {
-        System.setProperty("webdriver.chrome.driver", "/home/webonise/selenuim/selenium/chromedriver");
-        WebDriver driver;
-        driver = new ChromeDriver();
- 
-        driver.get("https://www.lenskart.com");
-        
-        WebElement  click_login = driver.findElement(By.xpath("//div[@class='header-content']/span[@class = 'login-text']"));
-        click_login.click();
-               
-        WebElement  signup = driver.findElement(By.xpath("//a[@id=\"acheckout\"]"));
-        signup.click();
-        
-        Thread.sleep(2000);
+	public static void main(String[] args) {
 
-        WebElement firstname = driver.findElement(By.xpath( "//form[@id=\"signup-lenskart\"]/ul/li[1]/input[@id=\"firstname\"]"));
-        firstname.sendKeys("Anthony");
-        
-        WebElement lastname = driver.findElement(By.xpath( "//form[@id=\"signup-lenskart\"]/ul/li[2]/input[@id=\"lastname\"]"));
-        lastname.sendKeys("Jones");
-        
-        WebElement mobile = driver.findElement(By.xpath( "//form[@id=\"signup-lenskart\"]/ul/li[3]/input[@id=\"confirmation_mobile\"]"));
-        mobile.sendKeys("8888947681");
-        
-        WebElement email = driver.findElement(By.xpath( "//form[@id=\"signup-lenskart\"]/ul/li[4]/input[@id=\"email_address\"]"));
-        email.sendKeys("Anthony.jones@gmail.com");
+		try {
+			System.setProperty("webdriver.chrome.driver", "/home/webonise/selenuim/selenium/chromedriver");
+			WebDriver driver;
+			driver = new ChromeDriver();
+			driver.get("https://www.lenskart.com");
 
-        WebElement password = driver.findElement(By.xpath( "//form[@id=\"signup-lenskart\"]/ul/li[5]/input[@id=\"password\"]"));
-        password.sendKeys("jones@94");
+			WebElement click_login = driver
+					.findElement(By.xpath("//div[@class='header-content']/span[@class = 'login-text']"));
+			click_login.click();
 
-        WebElement submit = driver.findElement(By.xpath("//form[@id=\"signup-lenskart\"]/ul/li[9]/div/a[@class=\"submit\"]"));
-        submit.click();
-        
-        Thread.sleep(3000);
-  
-        driver.quit();
-       }catch(Exception e) {
-    	 System.out.println(""+e);  
-       }
-    }
+			Thread.sleep(1000);
+
+			
+			WebElement signup = driver.findElement(By.xpath("//a[@class='top-link-signup']"));
+			signup.click();
+
+			Thread.sleep(2000);
+
+			WebElement firstname = driver
+					.findElement(By.xpath("//form[@id=\"signup-lenskart\"]/ul/li[1]/input[@id=\"firstname\"]"));
+			firstname.sendKeys("Swapnil");
+
+			WebElement lastname = driver
+					.findElement(By.xpath("//form[@id=\"signup-lenskart\"]/ul/li[2]/input[@id=\"lastname\"]"));
+			lastname.sendKeys("JEJURKAR");
+
+			WebElement mobile = driver.findElement(
+					By.xpath("//form[@id=\"signup-lenskart\"]/ul/li[3]/input[@id=\"confirmation_mobile\"]"));
+			mobile.sendKeys("8888876814");
+
+			WebElement email = driver
+					.findElement(By.xpath("//form[@id=\"signup-lenskart\"]/ul/li[4]/input[@id=\"email_address\"]"));
+			email.sendKeys("sjjejurkar@gmail.com");
+
+			WebElement password = driver
+					.findElement(By.xpath("//form[@id=\"signup-lenskart\"]/ul/li[5]/input[@id=\"password\"]"));
+			password.sendKeys("Swapnil@94");
+
+			WebElement submit = driver
+					.findElement(By.xpath("//form[@id=\"signup-lenskart\"]/ul/li[9]/div/a[@class=\"submit\"]"));
+			submit.click();
+
+			Thread.sleep(3000);
+			
+			WebElement menuList =  driver.findElement(By.id("TabbedPanels1"));
+		    menuList.click();
+		    List<WebElement>elems = driver.findElements(By.xpath("//ul[@id=\"cart-ajax\"]"));
+		      for (WebElement ddlList : elems)
+		      {
+		          System.out.println(ddlList.getText());
+		        //Something like ddlList.length(); but not able to work upon it.
+
+
+		     }
+		    
+			
+
+			driver.quit();
+		} catch (Exception e) {
+			System.out.println("" + e);
+		}
+	}
 
 	@Override
 	public void run() {
-	
-	}
- 
-}
 
+	}
+
+}
